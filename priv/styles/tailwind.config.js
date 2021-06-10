5e56ba27eb569fc4a5a2e532a4cac7d8abb7f2a3
Adam Lindberg <hello@alind.io>
2021-06-10 11:58:54 +0200

    Simplify Tailwind configuration

--- priv/styles/tailwind.config.js
+++ priv/styles/tailwind.config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  purge: [
-    '../web/templates/**/*.mustache',
-    '../web/static/**/*.html',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
+  purge: {
+    enabled: true,
+    content: [
+      '../web/templates/**/*.mustache',
+      '../web/static/**/*.html',
+    ]
   },
-  variants: {
-    extend: {},
-  },
+  theme: {},
+  variants: {},
   plugins: [],
 }
