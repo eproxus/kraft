daa7a6ec2897b60b016464e189a31e6f73f77a43
Adam Lindberg <hello@alind.io>
2021-09-15 10:04:35 +0200

    Avoid depending on Hyperscript in chat example

--- examples/chat/src/chat.erl
+++ examples/chat/src/chat.erl
@@ -1,19 +1,24 @@
 -module(chat).
 
 -behaviour(application).
+-behaviour(kraft_controller).
 
 % Callbacks
 -export([start/2]).
 -export([stop/1]).
+-export([init/2]).
 
 %--- Callbacks -----------------------------------------------------------------
 
 start(_StartType, _StartArgs) ->
     kraft:start(#{port => 8093}, [
         {"/chatroom", {ws, chat_room}, #{}, #{type => json}},
-        {"/", kraft_static, #{}}
+        {"/", ?MODULE, #{}}
     ]),
     chat_sup:start_link().
 
 stop(_State) ->
     kraft:stop().
+
+init(Conn, _Params) ->
+    {200, #{}, kraft:render(Conn, "index.html", #{})}.
