d71d8ea5880c930b77518d26e88148bb49a4f437
Adam Lindberg <hello@alind.io>
2021-12-28 19:15:29 +0100

    Add missing template example

--- examples/index/src/index.erl
+++ examples/index/src/index.erl
@@ -1,19 +1,25 @@
 -module(index).
 
 -behaviour(application).
+-behaviour(kraft_controller).
 
 % Callbacks
 -export([start/2]).
 -export([stop/1]).
+-export([init/3]).
 
 %--- Callbacks -----------------------------------------------------------------
 
 start(_StartType, _StartArgs) ->
     kraft:start(#{port => 8090}, [
         {"/", kraft_static, #{file => "index.html"}},
-        {"/error", module_does_not_exist, #{}}
+        {"/error", module_does_not_exist, #{}},
+        {"/missing_template", ?MODULE, #{}}
     ]),
     {ok, self()}.
 
 stop(_State) ->
     kraft:stop().
+
+init(Conn, _Params, _State) ->
+    {200, #{}, kraft:render(Conn, "missing", #{})}.
